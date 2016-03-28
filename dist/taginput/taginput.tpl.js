8bc9dd6f1762d752524e3c698ab5d091daa520bd
nuysoft <nuysoft@gmail.com>
2016-03-28 17:06:45 +0800

    Revert "Issues #4 #5 #6 #7 #8 #10 #11 #12"

--- dist/taginput/taginput.tpl.js
+++ dist/taginput/taginput.tpl.js
@@ -3,7 +3,7 @@ define(function() {
     return (function(){/*
 <div class="taginput" bx-click="_focus">
 	<div class="taginput-placeholder hide <%= data.length ? 'hide' : '' %>"><%= placeholder %></div>
-	<input bx-name="components/suggest" class="taginput-input" autocomplete="off">
+	<input bx-name="components/suggest" class="taginput-input">
 </div>
     */}).toString().split("\n").slice(1,-1).join("\n")
 })
